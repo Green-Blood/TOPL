87ca0697654e09088c3bec0e21482a2178f812ff
Green-Blood <bloodjey98@gmail.com>
2019-11-30 17:49:52 +0500

    Kaka

--- PiDE/testIDE.py
+++ PiDE/testIDE.py
@@ -1,21 +1,30 @@
-import tkinter
-from _tkinter import *
+from tkinter import *
 
-root = tkinter.Tk()
+root = Tk()
+root.geometry("600x400")
 
-label1 = tkinter.Label(text=" enter cluster : ")
+left = Frame(root, borderwidth=2, relief="solid")
+right = Frame(root, borderwidth=2, relief="solid")
+container = Frame(left, borderwidth=2, relief="solid")
+box1 = Frame(right, borderwidth=2, relief="solid")
+box2 = Frame(right, borderwidth=2, relief="solid")
+
+label1 = Label(container, text="I could be a canvas, but I'm a label right now")
+label2 = Label(left, text="I could be a button")
+label3 = Label(left, text="So could I")
+label4 = Label(box1, text="I could be your image")
+label5 = Label(box2, text="I could be your setup window")
+
+left.pack(side="left", expand=True, fill="both")
+right.pack(side="right", expand=True, fill="both")
+container.pack(expand=True, fill="both", padx=5, pady=5)
+box1.pack(expand=True, fill="both", padx=10, pady=10)
+box2.pack(expand=True, fill="both", padx=10, pady=10)
+
 label1.pack()
-clvar = tkinter.IntVar()
-cluster = tkinter.Entry(bd=5)
-clvar = cluster.get()
-cluster.pack()
+label2.pack()
+label3.pack()
+label4.pack()
+label5.pack()
 
-
-def open1():
-    print("K-MEDOIDS CLUSTERING")
-    exec('example.py')
-
-
-button_1 = tkinter.Button(text="SUBMIT", command=open1)
-button_1.pack()
 root.mainloop()
--- Project/PiDE/PiDEeditor.py
+++ Project/PiDE/PiDEeditor.py
@@ -1,19 +1,26 @@
-from tkinter import Tk, scrolledtext, Menu, filedialog, END, messagebox, simpledialog
+from tkinter import Label, Frame, Tk, scrolledtext, Menu, filedialog, END, messagebox, simpledialog
 import os
 import platform
 from tkinter.messagebox import showwarning
 import subprocess
+import sys
+
 # Main window
 root = Tk(className=" PiDE")
-textArea = scrolledtext.ScrolledText(root, width = 100, height = 80)
+left = scrolledtext.ScrolledText(root, width = 100, height = 80, borderwidth=2, relief="solid")
+
+# Splitting windows
+
+right = Frame(root, borderwidth=2, relief="solid")
+
 
 # Functions
 def newFile():
     # If there is content
-    if len(textArea.get('1.0', END+'-1c')) > 0:
+    if len(left.get('1.0', END+'-1c')) > 0:
         if messagebox.askyesno("Save?", "Do you wish to save?"):
             saveFile()
-        else: textArea.delete('1.0', END)
+        else: left.delete('1.0', END)
     root.title("PiDE")
 
 # Hard to say it, but this function will open a File
@@ -24,14 +31,14 @@ def openFile():
 
     if file != None:
         contents = file.read()
-        textArea.insert('1.0', contents)
+        left.insert('1.0', contents)
         file.close()
 
 def saveFile():
     file = filedialog.asksaveasfile(mode = 'w')
     # slice of the last character from get, as an extra return is added
     if file != None:
-        data = textArea.get('1.0', END+'-1c')
+        data = left.get('1.0', END+'-1c')
         file.write(data)
         file.close()
 def exitEditor():
@@ -39,7 +46,7 @@ def exitEditor():
         root.destroy()
 def findInFile():
     findString = simpledialog.askstring("Find...", "Enter text, to find ")
-    textData = textArea.get('1.0', END)
+    textData = left.get('1.0', END)
 
     occurances = textData.upper().count(findString.upper())
 
@@ -52,14 +59,24 @@ def runFile():
         file = filedialog.asksaveasfile(mode='w')
         # slice of the last character from get, as an extra return is added
         if file != None:
-            data = textArea.get('1.0', END + '-1c')
+            data = left.get('1.0', END + '-1c')
             file.write(data)
             file.close()
-        os.system("python PyScript " + os.path.basename(file.name))
-
+        # run file Take an output and write it into out.txt
+        out = "./PyScript " + os.path.basename(file.name) + ' > out.txt'
+        os.system(out)
+        # Open the out.txt file and read contents of it
+        f = open("out.txt", "r")
+        contents = f.read()
+        # Write context into IDE
+        shellLabel =  Label(right, text = contents)
+        # Close and delete the file 
+        f.close()
+        os.remove("out.txt")
+        shellLabel.pack()
 
 def about():
-    label = messagebox.showinfo("About", "This is a PiDE, which we created to help working with PieScript")
+    messagebox.showinfo("About", "This is a PiDE, which we created to help working with PieScript")
 def help():
     messagebox.showinfo("Help", "This is help Menu")
 def shell():
@@ -71,6 +88,8 @@ def shell():
         elif platform.system() == 'Darwin':
             os.system('open -a Terminal')
         root.destroy()
+
+
 
 
 
@@ -99,7 +118,10 @@ actionMenu.add_command(label = "Shell", command = shell)
 actionMenu.add_command(label = "Run", command = runFile)
 
 
-textArea.pack()
+left.pack(side="left", expand=True, fill="both")
+right.pack(side="right", expand=True, fill="both")
+
+
 
 # Keep window open
 root.mainloop()
